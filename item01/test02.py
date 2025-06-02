5693630a63bdb5ed48d086852e140d8a6c6d352e
LuckyQuill <1062260387@qq.com>
2025-06-02 21:39:00 +0800

    gitee commit2

--- item01/test02.py
+++ item01/test02.py
@@ -1,13 +1,15 @@
 math_list = [30, 60, 15, 45, 7, 1, 75, 20, 25]
+math2_list = []
+math3_list = []
 i = 1
 for t in math_list:
     print(f"现在是第{i}次")
     if i % 2 == 0:
         t *= 2
-        print(t)
+        math2_list.append(t)
     elif i % 3 == 0:
         t *= 3
-        print(t)
+        math3_list.append(t)
     else:
         print(t * 5)
     if t <= 30:
